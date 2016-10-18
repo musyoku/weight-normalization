12dd0d7d6089b194fc4a6515f7d4bc5f9266c9ff
musyoku <musyoku@users.noreply.github.com>
2016-10-18 12:19:49 +0900

    j

--- linear.py
+++ linear.py
@@ -3,6 +3,7 @@ from chainer import cuda
 from chainer import initializers
 from chainer import link
 from chainer import function
+from chainer.utils import array
 from chainer.utils import type_check
 
 def _as_mat(x):
@@ -10,51 +11,77 @@ def _as_mat(x):
 		return x
 	return x.reshape(len(x), -1)
 
+def norm_gpu(x):
+	x = array.as_mat(x)
+	l2norm_kernel = cuda.cupy.ReductionKernel(
+		"T x, float32 eps",
+		"T y",
+		"x * x",
+		"a + b",
+		"y = sqrt(a) + eps",
+		"0",
+		"l2norm"
+	)
+	norm = cuda.cupy.broadcast_to(
+		l2norm_kernel(x, 1e-5, axis=1).reshape(-1, 1),
+		x.shape
+	)
+	return norm
+
+cuda.cupy.linalg.norm = norm_gpu
+
 class LinearFunction(function.Function):
 
 	def forward(self, inputs):
 		x = _as_mat(inputs[0])
-		W = inputs[1]
+		V = inputs[1]
+		g = inputs[2]
+		xp = cuda.get_array_module(V)
+		self.norm_V = xp.linalg.norm(V)			# cache for backward operation
+		self.normalized_V = V / self.norm_V		# cache for backward operation
+		W = g * self.normalized_V
 		y = x.dot(W.T).astype(x.dtype, copy=False)
-		if len(inputs) == 3:
-			b = inputs[2]
+		if len(inputs) == 4:
+			b = inputs[3]
 			y += b
 		return y,
 
 	def backward(self, inputs, grad_outputs):
 		x = _as_mat(inputs[0])
 		W = inputs[1]
+		g = inputs[2]
 		gy = grad_outputs[0]
 
 		gx = gy.dot(W).astype(x.dtype, copy=False).reshape(inputs[0].shape)
 		gW = gy.T.dot(x).astype(W.dtype, copy=False)
+		gg = gW * self.normalized_V
+		gV = g * gW / self.norm_V - g * gg * self.normalized_V / self.norm_V
+
 		if len(inputs) == 3:
 			gb = gy.sum(0)
-			return gx, gW, gb
+			return gx, gV, gg, gb
 		else:
-			return gx, gW
+			return gx, gV, gg
 
-def linear(x, W, b=None):
+def linear(x, V, g, b=None):
 	if b is None:
-		return LinearFunction()(x, W)
+		return LinearFunction()(x, V, g)
 	else:
-		return LinearFunction()(x, W, b)
+		return LinearFunction()(x, V, g, b)
 
 class Linear(link.Link):
 
-	def __init__(self, in_size, out_size, wscale=1, bias=0, nobias=False,
-				 initialV=None, initial_bias=None):
+	def __init__(self, in_size, out_size, wscale=1, bias=0, nobias=False, initialV=None, initial_bias=None):
 		super(Linear, self).__init__()
 
 		self.initialV = initialV
 		self.wscale = wscale
-		self.initialG = 1
 
 		self.out_size = out_size
 		self._V_initializer = initializers._get_initializer(initialV, math.sqrt(wscale))
 
 		if in_size is None:
-			self.add_uninitialized_param("W")
+			self.add_uninitialized_param("V")
 		else:
 			self._initialize_params(in_size)
 
@@ -66,12 +93,13 @@ class Linear(link.Link):
 			bias_initializer = initializers._get_initializer(initial_bias)
 			self.add_param("b", out_size, initializer=bias_initializer)
 
+		self.add_param("g", 1, initializer=initializers._get_initializer(1))
+
 	def _initialize_params(self, in_size):
-		self.add_param("W", (self.out_size, in_size),
-					   initializer=self._V_initializer)
+		self.add_param("V", (self.out_size, in_size), initializer=self._V_initializer)
 
 	def __call__(self, x):
 		if self.has_uninitialized_params:
 			with cuda.get_device(self._device_id):
 				self._initialize_params(x.size // len(x.data))
-		return linear.linear(x, self.W, self.b)
+		return linear(x, self.V, self.g, self.b)
---
+++ test_linear.py
@@ -0,0 +1,160 @@
+import os
+import tempfile
+import unittest
+import numpy as np
+import chainer
+from chainer import cuda
+from chainer import gradient_check
+from chainer import links
+from chainer.serializers import npz
+from chainer import testing
+from chainer.testing import attr
+from chainer.testing import condition
+from chainer.utils import type_check
+
+@testing.parameterize(*testing.product({
+	"in_shape": [(3,), (3, 2, 2)],
+	"x_dtype": [np.float16, np.float32, np.float64],
+	"W_dtype": [np.float16, np.float32, np.float64],
+}))
+class TestLinear(unittest.TestCase):
+
+	out_size = 2
+
+	def setUp(self):
+		in_size = np.prod(self.in_shape)
+		self.link = links.Linear(
+			in_size, self.out_size,
+			initialW=chainer.initializers.Normal(1, self.W_dtype),
+			initial_bias=chainer.initializers.Normal(1, self.x_dtype))
+		W = self.link.W.data
+		b = self.link.b.data
+		self.link.cleargrads()
+
+		x_shape = (4,) + self.in_shape
+		self.x = np.random.uniform(-1, 1, x_shape).astype(self.x_dtype)
+		self.gy = np.random.uniform(
+			-1, 1, (4, self.out_size)).astype(self.x_dtype)
+		self.y = self.x.reshape(4, -1).dot(W.T) + b
+		self.check_forward_options = {}
+		self.check_backward_options = {}
+		if self.x_dtype == np.float16:
+			self.check_forward_options = {"atol": 1e-3, "rtol": 1e-2}
+			self.check_backward_options = {"atol": 1e-2, "rtol": 5e-2}
+		elif self.W_dtype == np.float16:
+			self.check_backward_options = {"atol": 1e-3, "rtol": 1e-2}
+
+	def check_forward(self, x_data):
+		x = chainer.Variable(x_data)
+		y = self.link(x)
+		self.assertEqual(y.data.dtype, self.x_dtype)
+		testing.assert_allclose(self.y, y.data, **self.check_forward_options)
+
+	@condition.retry(3)
+	def test_forward_cpu(self):
+		self.check_forward(self.x)
+
+	@attr.gpu
+	@condition.retry(3)
+	def test_forward_gpu(self):
+		self.link.to_gpu()
+		self.check_forward(cuda.to_gpu(self.x))
+
+	def check_backward(self, x_data, y_grad):
+		gradient_check.check_backward(
+			self.link, x_data, y_grad, (self.link.W, self.link.b), eps=2 ** -3,
+			**self.check_backward_options)
+
+	@condition.retry(3)
+	def test_backward_cpu(self):
+		self.check_backward(self.x, self.gy)
+
+	@attr.gpu
+	@condition.retry(3)
+	def test_backward_gpu(self):
+		self.link.to_gpu()
+		self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+
+
+class TestLinearParameterShapePlaceholder(unittest.TestCase):
+
+	in_size = 3
+	in_shape = (in_size,)
+	out_size = 2
+	in_size_or_none = None
+
+	def setUp(self):
+		self.link = links.Linear(self.in_size_or_none, self.out_size)
+		temp_x = np.random.uniform(-1, 1,
+									  (self.out_size,
+									   self.in_size)).astype(np.float32)
+		self.link(chainer.Variable(temp_x))
+		W = self.link.W.data
+		W[...] = np.random.uniform(-1, 1, W.shape)
+		b = self.link.b.data
+		b[...] = np.random.uniform(-1, 1, b.shape)
+		self.link.cleargrads()
+
+		x_shape = (4,) + self.in_shape
+		self.x = np.random.uniform(-1, 1, x_shape).astype(np.float32)
+		self.gy = np.random.uniform(
+			-1, 1, (4, self.out_size)).astype(np.float32)
+		self.y = self.x.reshape(4, -1).dot(W.T) + b
+
+	def check_forward(self, x_data):
+		x = chainer.Variable(x_data)
+		y = self.link(x)
+		self.assertEqual(y.data.dtype, np.float32)
+		testing.assert_allclose(self.y, y.data)
+
+	@condition.retry(3)
+	def test_forward_cpu(self):
+		self.check_forward(self.x)
+
+	@attr.gpu
+	@condition.retry(3)
+	def test_forward_gpu(self):
+		self.link.to_gpu()
+		self.check_forward(cuda.to_gpu(self.x))
+
+	def check_backward(self, x_data, y_grad):
+		gradient_check.check_backward(
+			self.link, x_data, y_grad, (self.link.W, self.link.b), eps=1e-2)
+
+	@condition.retry(3)
+	def test_backward_cpu(self):
+		self.check_backward(self.x, self.gy)
+
+	@attr.gpu
+	@condition.retry(3)
+	def test_backward_gpu(self):
+		self.link.to_gpu()
+		self.check_backward(cuda.to_gpu(self.x), cuda.to_gpu(self.gy))
+
+	def test_serialization(self):
+		lin1 = links.Linear(None, self.out_size)
+		x = chainer.Variable(self.x)
+		# Must call the link to initialize weights.
+		lin1(x)
+		w1 = lin1.W.data
+		fd, temp_file_path = tempfile.mkstemp()
+		os.close(fd)
+		npz.save_npz(temp_file_path, lin1)
+		lin2 = links.Linear(None, self.out_size)
+		npz.load_npz(temp_file_path, lin2)
+		w2 = lin2.W.data
+		self.assertEqual((w1 == w2).all(), True)
+
+
+class TestInvalidLinear(unittest.TestCase):
+
+	def setUp(self):
+		self.link = links.Linear(3, 2)
+		self.x = np.random.uniform(-1, 1, (4, 1, 2)).astype(np.float32)
+
+	def test_invalid_size(self):
+		with self.assertRaises(type_check.InvalidType):
+			self.link(chainer.Variable(self.x))
+
+
+testing.run_module(__name__, __file__)
